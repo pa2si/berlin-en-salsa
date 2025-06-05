fc8c78df774992b67f07c7ced2e43aaadf56bbf1
pa2si <pa2sicoding@gmail.com>
2025-06-05 21:05:21 +0200

    first commit under construction

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
           email:{" "}
           <a
             href="mailto:kontakt@berlinensalsa.de"
-            className="text-blue-600 hover:text-blue-800 hover:underline"
+            className="text-bes-red hover:text-blue-800 hover:underline"
           >
             kontakt@berlinensalsa.de
           </a>
