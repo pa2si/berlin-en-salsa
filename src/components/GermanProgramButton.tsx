bb590e832ea8f071317800db7422f240ab4bc4f7
pa2si <pa2sicoding@gmail.com>
2025-06-28 01:49:58 +0200

    v 0.1.14  - now really saved

--- src/components/GermanProgramButton.tsx
+++ src/components/GermanProgramButton.tsx
@@ -8,7 +8,7 @@ const GermanProgramButton = ({ onClick }: { onClick: () => void }) => {
 
   return (
     <motion.button
-      className="bg-bes-red fixed right-6 bottom-6 z-40 flex items-center space-x-2 rounded-full px-3 sm:px-4 py-3 shadow-lg"
+      className="bg-bes-red fixed right-6 bottom-6 z-40 flex items-center space-x-2 rounded-full px-3 py-3 shadow-lg sm:px-4"
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
       onMouseEnter={() => setIsHovered(true)}
@@ -18,23 +18,25 @@ const GermanProgramButton = ({ onClick }: { onClick: () => void }) => {
       animate={{ y: 0, opacity: 1 }}
       transition={{ delay: 10, type: "spring" }}
     >
-      <motion.svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        className="h-6 w-6 text-bes-amber" 
-        fill="none" 
-        viewBox="0 0 24 24" 
+      <motion.svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="text-bes-amber h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
         stroke="currentColor"
         animate={isHovered ? { rotate: 360 } : {}}
         transition={{ duration: 1.5, ease: "linear" }}
       >
-        <path 
-          strokeLinecap="round" 
-          strokeLinejoin="round" 
-          strokeWidth={2} 
-          d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" 
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
         />
       </motion.svg>
-      <span className="text-bes-amber font-bold hidden sm:inline">Programm Ansehen</span>
+      <span className="text-bes-amber hidden font-bold sm:inline">
+        Programm Ansehen
+      </span>
     </motion.button>
   );
 };
