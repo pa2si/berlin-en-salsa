a8e3b23d4ef35899be8ec98fc26c721f87410030
pa2si <pa2sicoding@gmail.com>
2026-03-28 23:23:51 -0500

    ver programa false

--- src/config/festival.ts
+++ src/config/festival.ts
@@ -97,7 +97,7 @@ export const FESTIVAL_CONFIG = {
    * Set to true when the timetable is finalized and ready to be displayed
    */
   timetable: {
-    isAvailable: true, // Change to true when timetable is ready
+    isAvailable: false, // Change to true when timetable is ready
   },
 
   /**
